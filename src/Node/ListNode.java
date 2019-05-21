be1803757db8308ea2680c9f9c7107db986b4c78
gravityc95 <gravityc95@gmail.com>
2019-05-21 18:45:55 +0900

    uncomment

--- src/Node/ListNode.java
+++ src/Node/ListNode.java
@@ -12,17 +12,6 @@ public interface ListNode extends Node{
 		}
 	};
 	
-//	static ListNode END_LIST = new ListNode() {
-//		@Override
-//		public Node car() {
-//			return null;
-//		}
-//		@Override
-//		public ListNode cdr() {
-//			return null;
-//		}
-//	};
-	
 	static ListNode cons(Node head, ListNode tail) {
 		return new ListNode() {
 			@Override
